5bfbeb2f42beae8c5dfca7374ec9290d3794fd24
Yash Mishra <136472014+yashmishra11@users.noreply.github.com>
2025-09-17 17:11:11 +0530

    Update sumandavgofarray.java

--- sumandavgofarray.java
+++ sumandavgofarray.java
@@ -1,13 +1,13 @@
 public class sumandavgofarray {
     public static void main(String[] args) {
-        int[] arr = { 1, 2, 3, 4, 5 };
+        int[] arr = { 11, 12, 18, 14 };
         int length = arr.length;
         int sum = 0;
         for (int i = 0; i < length; i++) {
             sum = sum + arr[i];
         }
         System.out.println("sum of element of array is :" + sum);
-        float avg = sum / length;
+        float avg = (float) sum / length;
         System.out.println("average of element of array is :" + avg);
     }
 }
