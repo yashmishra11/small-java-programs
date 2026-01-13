ad1b2401f3db84a800112a61f494921c262bc011
Yash Mishra <136472014+yashmishra11@users.noreply.github.com>
2026-01-13 14:19:53 +0530

    Update and rename avgofNnum.java to avgofNnums.java

--- avgofNnums.java
+++ avgofNnums.java
@@ -1,19 +1,22 @@
 import java.util.Scanner;
 
-public class avgofnum {
+public class avgofNnums {
     public static void main(String[] args) {
         int count;
         System.out.println("enter the count of numbers: ");
         Scanner sc = new Scanner(System.in);
         count = sc.nextInt();
+        System.out.println("enter the numbers: ");
+
         int number, sum = 0;
         float average = 0;
         for (int i = 0; i < count; i++) {
             number = sc.nextInt();
             sum = sum + number;
         }
-        average = sum / count;
+        average = (float) sum / count;
         System.out.println("the average is: " + average);
+        sc.close();
     }
 
 }
